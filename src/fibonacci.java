1cddc0707f82d1829f0973189710915321a03399
laura vanesa <castro.arce@outllok.es>
2022-07-01 15:27:18 -0500

    este es un cambio

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,2 +1,19 @@
 public class fibonacci {
-}
+    public static void main(String[] args) {
+       // ingresar n numeros de la serie fibonacci5
+        int I,a = 0,b = 1,c,n = 0;
+        System.out.println("ingresar el valor de n: ");
+        for (int i = 1; i <= n; i ++){
+
+            System.out.println("a");
+            c = a +b;
+            a = b;
+            b = c;
+
+
+
+            }
+
+        }
+    }
+
